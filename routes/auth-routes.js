33113c0832cca24660b5c3decbeef348c8d0ed3c
su <jeongyongsu5366@gmail.com>
2019-07-22 18:55:55 +0900

    Check login Data from login.ejs's form tag's POST method
    - This router will only be used for the clients who have registered this site locally
    
    - 1. Check ths login Data is valid using loginValidation method
    - 2. Check the login Data is matched with the data in Database
    - 3. Create JWT named auth_token
    - 4. send it to the user's browser
    - 5. render('homepage')
    - 6. Now this user has right to access his/her own profile page

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -62,3 +62,26 @@ router.post('/register', async (req, res) => {
 		res.status(400).send(err);
 	}
 });
+
+router.post('/login', async (req, res) => {
+	const { error } = loginValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	const user = await localUser.findOne({ email: req.body.email });
+	if (!user) return res.status(400).send('Email or Passowrd is wrong');
+	// Check if password is correct
+	const validPassword = await bcrypt.compare(req.body.password, user.password);
+	if (!validPassword) return res.status(400).send('Invalid Password');
+
+	// Right After validating Password, we're going to res.send jwt
+	// res.send('Logged in'!);
+	const payload = {
+		_id: user._id,
+		isAdmin: user.isAdmin,
+		email: user.email
+	};
+	const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: 86400 });
+	res.cookie('auth_token', token, { maxAge: 86400, httpOnly: true });
+	console.log(user);
+	res.status(200).render('homepage', { user: user });
+});
