f21a87e0d68d5a4c40ec316c5c25b7c71f882918
su <jeongyongsu5366@gmail.com>
2019-07-22 18:19:38 +0900

    Define GoogleStrategy Callback function
    - I have categorized two cases: those who have signed up before and
    those who have never signed up.
    - After updating the logined data in Database, done method is going to
    operate.
    - Done is a method called internally by the strategy implementation. It
    navigates you, one of the success/error/fail methods. Each of these
    options may calls to the next.When success is called, it can attach the
    user to the request or do other things depending on your needs, and send
    it to serialize function that I'm going to define

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -6,10 +6,34 @@ const googleUser = require('../models/googleUser');
 dotenv.config();
 
 passport.use(
-	new GoogleStrategy({
-		// options for google strategy
-		clientID: process.env.clientID,
-		clientSecret: process.env.clientSecret,
-		callbackURL: '/auth/google/redirect'
-	})
+	new GoogleStrategy(
+		{
+			// options for google strategy
+			clientID: process.env.clientID,
+			clientSecret: process.env.clientSecret,
+			callbackURL: '/auth/google/redirect'
+		},
+		(accessToken, refreshToken, profile, done) => {
+			// check if user already exists in our own db
+			googleUser.findOne({ googleId: profile.id }).then(currentUser => {
+				if (currentUser) {
+					// already have this user
+					console.log('user is: ', currentUser);
+					done(null, currentUser);
+				} else {
+					// if not, create user in our db
+					new googleUser({
+						googleId: profile.id,
+						username: profile.displayName,
+						email: profile.email
+					})
+						.save()
+						.then(newUser => {
+							console.log('created new user: ', newUser);
+							done(null, newUser);
+						});
+				}
+			});
+		}
+	)
 );
