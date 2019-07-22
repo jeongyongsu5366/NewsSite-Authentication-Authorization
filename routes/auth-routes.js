0d63378ad7cc72f19b08a4bef8513929905475ce
su <jeongyongsu5366@gmail.com>
2019-07-22 17:59:23 +0900

    Render login.ejs
    - When a requestor or clinet sends a request message to the url /login,
    I am going to return login.ejs using res.render method

---
+++ routes/auth-routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const { localUser, registerValidation, loginValidation } = require('../models/localUser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const router = express.Router();
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+router.get('/login', (req, res) => {
+	res.render('login', { user: req.user });
+});
